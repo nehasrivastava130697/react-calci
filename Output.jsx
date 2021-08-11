b561d2e397468ef21dc45e4c0ef08db1d31bab9c
nehasrivastava130697 <srivneha13@gmail.com>
2021-08-11 16:29:03 +0530

    Add files via upload

---
+++ Output.jsx
@@ -0,0 +1,12 @@
+
+
+let Output=(props)=>
+{
+    return( <input
+        className="output"
+          type="text"
+       value={props.result}
+          
+          />);
+}
+export default Output;
